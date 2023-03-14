3b249242d75fb26e0f21b2255b44ffafcd0faad4
Joshua Porth <joshuaporth@outlook.com>
2023-03-14 10:54:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bitcoin-liquid-supply
-Measuring the liquid supply of Bitcoin 🛢️🛢️🛢️
+Measuring the liquid supply of Bitcoin ⚗️
+
+## TODO
+- [ ] Define and document "Liquid Supply"
+- [ ] Convert LevelDB Sat Seconds Destroyed data to csv
+- [ ] Create Liquid Supply chart using [Dash][dash]
+- [ ] Attempt to reduce runtime further
+
+[dash]: https://dash.plotly.com/introduction
---
+++ db.py
@@ -0,0 +1,29 @@
+import pickle
+import plyvel
+
+class DB:
+	class Key:
+		def __init__(self, prefix: str, txid: str, vout: int) -> None:
+			self.bytes = f'{prefix}-{txid}-{vout}'.encode('ascii')
+		
+		def __eq__(self, other: object) -> bool:
+			return self.bytes == other.bytes
+		
+		def __hash__(self) -> int:
+			return self.bytes.__hash__()
+
+	def __init__(self, db_dir: str) -> None:
+		self._db = plyvel.DB(db_dir, create_if_missing=True)
+	
+	def get(self, key: Key) -> any:
+		data = self._db.get(key.bytes)
+		if data:
+			return pickle.loads(data)
+		return None
+
+	def batch(self, puts: list[tuple[Key, any]], deletes: list[Key]) -> None:
+		with self._db.write_batch() as wb:
+			for key, data in puts:
+				wb.put(key.bytes, pickle.dumps(data))
+			for key in deletes:
+				wb.delete(key.bytes)
